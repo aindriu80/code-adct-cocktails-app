aab6aef53b652645d1a52a3fa213d8241fa57026
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2022-05-02 10:26:32 +0100

    Search functionality

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -3,7 +3,33 @@ import { useGlobalContext } from '../context';
 
 const SearchForm = () => {
   const { setSearchTerm } = useGlobalContext();
-  return <div>SearchForm Component</div>
+  const searchValue = React.useRef('');
+
+  React.useEffect(() => {
+    searchValue.current.focus();
+  }, []);
+
+  const searchCocktail = () => {
+    setSearchTerm(searchValue.current.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <section className='section search'>
+      <form className='search-form' onSubmit={handleSubmit}>
+        <div className='form-control'>
+          <label htmlFor='name'>Search your favourite cocktail</label>
+          <input
+            type='text'
+            id='name'
+            ref={searchValue}
+            onChange={searchCocktail}
+          />
+        </div>
+      </form>
+    </section>
+  );
 }
 
 export default SearchForm
